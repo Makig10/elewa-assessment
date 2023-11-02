a8c77a356e8d59d2373e687f45f5f74c66d282af
Brian <mahuibrian14@gmail.com>
2023-11-03 01:04:58 +0300

    ✨:generated component for the social impact page

---
+++ src/app/social-impact-data/social-impact-data.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-social-impact-data',
+  templateUrl: './social-impact-data.component.html',
+  styleUrls: ['./social-impact-data.component.css']
+})
+export class SocialImpactDataComponent {
+
+}
