42de7973feb1a42c2b0ef861375721a61178ee0b
Brian <mahuibrian14@gmail.com>
2023-11-03 17:11:02 +0300

    ♻️: setting up the content section in the hero

--- src/app/invest-hero-content/invest-hero-content.component.ts
+++ src/app/invest-hero-content/invest-hero-content.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./invest-hero-content.component.css']
 })
 export class InvestHeroContentComponent {
-
+subtitle:string="Trade, not aid";
+title:string="Southern innovations addressing global problems"
 }
